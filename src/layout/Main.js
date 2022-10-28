4cd2a15ccd39fb117677afd0113e2e1e9b3b433d
Abdulkarim6 <mdhossainjwel687@gmail.com>
2022-10-28 18:57:46 +0600

    side responsive done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,16 +4,12 @@ import Footer from '../shared/footer/Footer';
 import Header from '../shared/header/Header';
 import SideNav from '../shared/sideNav/SideNav';
 import { Outlet } from 'react-router-dom'
-// import { useContext } from 'react';
-// import { AuthContext } from '../context/AuthProvider';
+
 
 const Main = () => {
-    // const {user} = useContext(AuthContext);
 
     return (
         <div>
-            {/* <h1>{user.displayName}</h1>
-            <h4>{user.age}</h4> */}
             <Header></Header>
             <Container>
                 <Row>
